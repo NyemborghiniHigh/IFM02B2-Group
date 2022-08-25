bf04fe10deb82da08227e75f9ff4755d128b859b
NyemborghiniHigh <110979033+NyemborghiniHigh@users.noreply.github.com>
2022-08-26 00:53:44 +0200

    Delete Service/Products directory

--- Service/Products/SQL_Scripts/dbo.Product.data.sql
+++
@@ -1,8 +0,0 @@
-﻿INSERT INTO Product(Brand_Name, Prod_Name, Description, Type_ID, Prod_Image, Active, Price, created_at, Quantity) VALUES
-('Ultra Dog', 'Superwoof Chicken Adult Dog', 'Ultra Dog Superwoof Beef and Rice Small-Medium Adult Dog Food is a high-quality dog food that has been scientifically developed to contain all of the necessary nutrients that dogs need to live their lives to the fullest',
-2, 'images/ultradog_adult_chicken.jpg', 1, 600, '2022-08-25', 12),
-('Rawsons', 'Birdy Pure Chicken 1kg', 'Our Birdy Pure Chicken contains a rich blend of chicken muscle meat, chicken organ and finely crushed bone.
-This meal can help build lean muscle mass and tone, and can be fed to dogs to lose some extra weight.
-Our Birdy Pure Chicken contains a rich source of protein and amino acids.
-Chicken provides omega 6 fatty acids which helps your dogs skin to be healthier and makes their coat shine.',
-1, 'images/birdy_raw.png', 1, 25, '2022-08-25', 10);
