2d7ed43a76546d0e0965b0acd139da369f6188fb
NyemborghiniHigh <110979033+NyemborghiniHigh@users.noreply.github.com>
2022-08-26 00:56:45 +0200

    Add files via upload

---
+++ Service/Products/SQL_Scripts/dbo.Product.data.sql
@@ -0,0 +1,39 @@
+﻿INSERT INTO Product(Brand_Name, Prod_Name, Description, Category, Prod_Image, Active, Price, created_at, Quantity, Animal_Name) VALUES
+('Amigo', 'Integrity Adult Dog Food 8kg', 'Amigo Integrity Small Senior Dog Food is a premium, steam cooked food made with no artificial colorants and is free of wheat gluten to avoid allergies and improve well-being.',
+'Kibble', 'images/integrity_dog.jpg', 1, 950 ,'2022-08-15', 14, 'Dog'),
+('Weruva', 'Mack & Jack Canned Cat Food', 'The Weruva Mack & Jack Canned Cat Food is made with mackerel and skipjack tuna. With the United States consuming nearly a third of the world’s catch, simply put, skipjack is the most popularly consumed tuna in the world, surpassing albacore nearly eight times over.',
+'Canned', 'images/weruva_cat.jpg', 1, 40, '2022-08-16', 50, 'Cat'),
+('NutriBird', 'Verse-Laga Nutribird P15 Original Parakeet Maintenance Pellets', 'Verse-Laga Nutribird P15 Original Parakeet Maintenance Pellets is a complete balanced maintenance food for large parakeets. It contains Flora Stimuli  - which promotes the intestinal flora as well as protecting against intestinal disorders. ',
+'Seeds', 'images/bird_seed.jpg', 1, 375, '2022-06-29', 6, 'Bird'),
+('Zilla', 'Zilla Reptile Food Munchies Mealworms', 'Nutritious, dehydrated ingredients that can be fed with other diets or as a treat
+Quick and easy way to provide reptiles with high protein foods that they prefer
+No refrigeration required
+Ideal for bearded dragons, leopard geckos, chameleons as well as large tropical fish and birds
+For smaller sized animals, break Mealworms into pieces before feeding', 'Dry Food', 'images/lizard_worms.jpg', 1, 405, '2022-08-02', 8, 'Reptile'),
+('Tetra', 'Tetra TetraFin Goldfish Flakes 7.06 Ounces, Balanced Diet Fish Food', 'Nutritionally-fortified flake food for ornamental goldfish.
+ProCare formulation helps promote better goldfish health.
+Staple diet helps improve goldfish color and growth. 
+Highly palatable.', 'Dry Food', 'images/fish_flakes.jpg', 1, 150, '2022-08-12', 6, 'Fish');
+('Awvolution', 'Pure Lamb 1kg', 'Our Lambo Lamb contains lamb muscle meat, organs as well as green tripe and crushed bone. It’s rich in pro-biotics, for an easier digestion.
+It’s a nutritious meal which is high in protein.
+Our Lambo Lamb can be fed to pregnant dogs as it strengthens their reproductive system. As well as for male dogs to boost testosterone and increase sperm count.',
+'Raw', 'images/lamb.png', 1, 40, '2022-07-15', 24, 'Dog'),
+('Awvolution', 'Months Supply Value Pack', '5 x 1kg Game Strong
+5 x 1kg Beef Supreme
+5 x 1kg Lambo Lamb
+5 x 1kg Leo Optimum
+10 x 1kg Birdy Chicken
+1 x 1.5kg Pure Chicken Chunks
+1 x 1kg Chicken Bone Broth
+1 x 1kg Game Strong Bone Broth
+1 x 1kg Beef Bone Broth', 'Raw', 'images/supply_pack_dog.png', 1, 1500, '2022-08-23', 3, 'Dog');
+
+('Awvolution', 'Birdy Pure Patties 7kg', 'Our Birdy Pure Chicken Patties contains a rich blend of chicken muscle meat, chicken organ and finely crushed bone.
+This meal can help build lean muscle mass and tone, and can be fed to dogs to lose some extra weight.
+Our Birdy Pure Chicken Patties contains a rich source of protein and amino acids.
+Chicken provides omega 6 fatty acids which helps your dogs skin to be healthier and makes their coat shine.', 'Raw', 'images/birdy_patties_cat.png', 1, 200, '2022-08-23', 9, 'Cat'),
+('Awvolution', 'Rabbit Mince 1.5kg', 'Our Whole-Prey Rabbit Mince Chunks contains a rich blend of rabbit meat, and rabbit organs.
+It’s best suited for cats, or kittens even to smaller dogs.
+This white meat is high in protein, rich in nutrients and essential vitamins and minerals.
+Our Pure Rabbit Chunks can be given to bigger dogs as a treat.
+Rabbit meat is also a novel protein.', 'Raw', 'images/rabbit_cat.png', 1, 230, '2022-08-20', 12, 'Cat');
