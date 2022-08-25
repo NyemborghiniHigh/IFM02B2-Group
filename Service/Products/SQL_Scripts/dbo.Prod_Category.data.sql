f97efec8d4a40cd389817817a7e2325dcf492302
NyemborghiniHigh <110979033+NyemborghiniHigh@users.noreply.github.com>
2022-08-25 12:01:09 +0200

    Add files via upload

---
+++ Service/Products/SQL_Scripts/dbo.Prod_Category.data.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO Prod_Category(Type_ID, Animal_Type, Produce_Type) VALUES
+(1, 'Dog', 'Raw'),
+(2, 'Dog', 'Kibble'),
+(3, 'Dog', 'Canned'),
+(4, 'Cat', 'Raw'),
+(5, 'Cat', 'Kibble'),
+(6, 'Cat', 'Canned');
